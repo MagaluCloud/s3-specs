c2f8f8736874f5646d74f27c6da29675b1af1da3
Fabricio C Zuardi <fabricio@fabricio.org>
2024-11-05 17:26:37 -0300

    list-buckets, new tests for cli tools rclone and awscli

--- docs/list-buckets_test.py
+++ docs/list-buckets_test.py
@@ -9,10 +9,8 @@
 
 # # List buckets
 #
-# List all buckets from a profile[<sup>1</sup>](../glossary#profile)
+# Lista os buckets de um perfil[<sup>1</sup>](../glossary#profile)
 
-
-# ## Examples
 
 # + tags=["parameters"]
 config = "../params/br-ne1.yaml"
@@ -22,11 +20,20 @@ docs_dir = "."
 # +
 import random
 import logging
+import subprocess
+import pytest
+from shlex import split, quote
 from s3_helpers import run_example
 # -
 
+# ## Exemplos
+#
+# ### Boto3
+#
+# O comando para listar buckets no boto3 é o `list_buckets`.
+
 # +
-def test_list_buckets(s3_client, profile_name="default"):
+def test_boto_list_buckets(s3_client, profile_name):
     response = s3_client.list_buckets()
     response_status = response["ResponseMetadata"]["HTTPStatusCode"]
     assert response_status == 200, "Expected HTTPStatusCode 200 for successful bucket list."
@@ -41,10 +48,36 @@ def test_list_buckets(s3_client, profile_name="default"):
         assert isinstance(bucket_name, str) and bucket_name, "Expected bucket name to be a non-empty string."
         logging.info(f"One of those buckets is named {random.choice(buckets).get('Name')}")
 
-run_example(__name__, "list-buckets", "test_list_buckets", config=config, docs_dir=docs_dir)
+run_example(__name__, "list-buckets", "test_boto_list_buckets", config=config, docs_dir=docs_dir)
+# -
+# ### Rclone e AWS CLI
+#
+# O comando para listar buckets no rclone é o `lsd`.
+# Os comandos para listar buckets na awscli são `s3 ls` e `s3api list-buckets`.
+#
+# **Exemplos:**
+
+commands = [
+    "rclone lsd {profile_name}:",
+    "aws s3 ls --profile {profile_name}",
+    "aws s3api list-buckets --profile {profile_name}",
+]
+
+# +
+@pytest.mark.parametrize("cmd_template", commands)
+def test_cli_list_buckets(cmd_template, profile_name):
+    cmd = split(cmd_template.format(profile_name=profile_name))
+    result = subprocess.run(cmd, capture_output=True, text=True)
+
+    assert result.returncode == 0, f"Command failed with error: {result.stderr}"
+    logging.info(f"Output from {cmd_template}: {result.stdout}")
+
+run_example(__name__, "list-buckets", "test_cli_list_buckets", config=config, docs_dir=docs_dir)
 # -
 
-
-# ## References
+# ## Referências
 #
 # - [Boto3 Documentation: list_bucket](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3/client/list_buckets.html)
+# - [rclone lsd](https://rclone.org/commands/rclone_lsd/)
+# - [aws cli ls](https://docs.aws.amazon.com/cli/latest/reference/s3/ls.html)
+# - [aws cli list-buckets](https://docs.aws.amazon.com/cli/latest/reference/s3api/list-buckets.html)
