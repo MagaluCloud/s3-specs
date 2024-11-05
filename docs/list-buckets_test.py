cab1b0ee9839927b41d4752932be89e715f0568a
Fabricio C Zuardi <fabricio@fabricio.org>
2024-11-05 15:47:36 -0300

    refactor list-buckets test to use run_example

--- docs/list-buckets_test.py
+++ docs/list-buckets_test.py
@@ -8,50 +8,40 @@
 # ---
 
 # # List buckets
-# List all buckets from a profile[<sup>1</sup>](./glossary#profile)
+#
+# List all buckets from a profile[<sup>1</sup>](../glossary#profile)
 
+
+# ## Examples
 
 # + tags=["parameters"]
-profile_name = "default"
+config = "../params/br-ne1.yaml"
+docs_dir = "."
 # -
-
-
-# + [markdown] jp-MarkdownHeadingCollapsed=true
-# ## Setup
-
 
 # +
 import random
-
-if __name__ != "__main__":
-    import pytest
-else:
-    from s3_helpers import print_timestamp, create_s3_client
-    print_timestamp()
-    s3_client = create_s3_client(profile_name)
+import logging
+from s3_helpers import run_example
 # -
-
-
-# ## Example
-
 
 # +
 def test_list_buckets(s3_client, profile_name="default"):
     response = s3_client.list_buckets()
+    response_status = response["ResponseMetadata"]["HTTPStatusCode"]
+    assert response_status == 200, "Expected HTTPStatusCode 200 for successful bucket list."
     buckets = response.get('Buckets')
-
     assert isinstance(buckets, list), "Expected 'Buckets' to be a list."
     buckets_count = len(buckets)
     assert isinstance(buckets_count, int), "Expected buckets count to be an integer."
-    print(f"Profile '{profile_name}' has {buckets_count} buckets.")
+    logging.info(f"Bucket list returned with status {response_status} and a list of {buckets_count} buckets")
 
     if buckets_count > 0:
         bucket_name = random.choice(buckets).get('Name')
         assert isinstance(bucket_name, str) and bucket_name, "Expected bucket name to be a non-empty string."
-        print(f"One of those buckets is named {random.choice(buckets).get('Name')}")
+        logging.info(f"One of those buckets is named {random.choice(buckets).get('Name')}")
 
-if __name__ == "__main__":
-    test_list_buckets(s3_client, profile_name)
+run_example(__name__, "list-buckets", "test_list_buckets", config=config, docs_dir=docs_dir)
 # -
 
 
