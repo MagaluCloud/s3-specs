1d5135bb29e5399835d7a9d9d6056987b82d3459
Fabricio C Zuardi <fabricio@fabricio.org>
2025-01-28 11:23:27 -0300

    refactor: improve policies_test notebook
    The goal of this project is to have descriptive tests, with
    markdown paragraphs explaining the examples, and not only
    pytests that runs but are undocumented.
    
    This patch improves one of our documents related to bucket
    policy, the policies_test.py one.

--- docs/policies_test.py
+++ docs/policies_test.py
@@ -6,6 +6,17 @@
 #   language_info:
 #     name: python
 # ---
+
+# # Bucket Policy (política de bucket)
+# 
+# Buckets de armazenamento por padrão são acessíveis apenas pela dona da conta que criou este 
+# recurso. Também por padrão, esta dona possui a permissão de executar **qualquer** operação
+# neste bucket e nos seus objetos. Configurar uma política de bucket é uma maneira de
+# modificar estas permissões padrões, seja para **restringir** de forma granular o número de operações
+# que podem ser executadas em um bucket ou objeto, e por quais contas ("Principals"), seja para
+# **conceder** mais acessos a determinados recursos, e para quais contas.
+
+# + {"jupyter": {"source_hidden": true}}
 import pytest
 import logging
 from botocore.exceptions import ClientError
@@ -13,10 +24,31 @@ from s3_helpers import(
     change_policies_json,
 )
 
-# # Policy Tests
 pytestmark = pytest.mark.policy
+# -
 
-# ### Test Variables
+# Políticas de bucket são descritas por meio de arquivos no formato JSON, que seguem uma gramática
+# específica, devem conter uma lista de regras _Statement_ onde cada ítem desta lista descreve um
+# `Effect` (`Allow` ou `Deny`), um campo `Principal` para descrever a(s) beneficiária(s) da regra
+# um campo `Action` com as operações que esta regra permite ou nega e um campo `Resource`, que
+# define a qual recurso esta regra se aplicará. As regras desta sintaxe pode ser consultada no
+# documento [Estrutura de uma Bucket Policy](https://docs.magalu.cloud/docs/storage/object-storage/access-control/bucket_policy_overview#estrutura-de-uma-bucket-policy)
+# Abaixo um modelo de documento de política sem os campos preenchidos:
+
+policy_dict_template = {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "",
+            "Principal": "",
+            "Action": "",
+            "Resource": ""
+        }
+    ]
+}
+
+# + {"jupyter": {"source_hidden": true}}
+# Exemplos de documentos inválidos:
 malformed_policy_json ='''{
     "Version": "2012-10-18",
     "Statement": [
@@ -42,7 +74,6 @@ misspeled_policy = """{
     ]
 }
 """
-
 wrong_version_policy = """{
     "Version": "2012-10-18",
     "Statement": 
@@ -56,27 +87,19 @@ wrong_version_policy = """{
 }
 """
 
-policy_dict_template = {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "",
-            "Principal": "",
-            "Action": "",
-            "Resource": ""
-        }
-    ]
-}
-
-
 cases = [
     *[(case, "MalformedJSON") for case in ['', 'jason',"''", misspeled_policy, wrong_version_policy]],
     *[(case, "MalformedPolicy") for case in ['{}','""', malformed_policy_json]],
  ]   
+# -
+
+# ## Atribuindo uma política a um bucket usando Python
+#
+# O método para atribuir uma bucket policy na biblioteca boto3 é o [put_bucket_policy](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3/client/put_bucket_policy.html), se o documento de política for válido o retorno trará um HTTPStatusCode `204` enquanto
+# se o documento for inválido por algum motivo uma _exception_ do tipo `ClientError` será levantada,
+# como mostram os testes abaixo:
 
 @pytest.mark.parametrize('input, expected_error', cases)
-
-# ## Asserting the possible combinations that raise errors on put_bucket_policy
 def test_put_invalid_bucket_policy(s3_client, existing_bucket_name, input, expected_error):
     try:
         s3_client.put_bucket_policy(Bucket=existing_bucket_name, Policy=input)
@@ -91,7 +114,6 @@ def test_put_invalid_bucket_policy(s3_client, existing_bucket_name, input, expec
     {"policy_dict": policy_dict_template, "actions": "s3:GetObject", "effect": "Deny"},
     {"policy_dict": policy_dict_template, "actions": "s3:DeleteObject", "effect": "Deny"}
 ])
-# ## Base case of putting a policy into a bcuket
 def test_setup_policies(s3_client, existing_bucket_name, policies_args):
     bucket_name = existing_bucket_name
 
@@ -99,18 +121,20 @@ def test_setup_policies(s3_client, existing_bucket_name, policies_args):
     policies = change_policies_json(existing_bucket_name, policies_args, "*")
     response = s3_client.put_bucket_policy(Bucket=bucket_name, Policy=policies) 
     assert response['ResponseMetadata']['HTTPStatusCode'] == 204
-    
-# # Tests related to actions on the bucket
+
+# ## Negar operações específicas em objetos
+#
+# Regras com o `Effect` `Deny` impedem que determinadas operações sejam executadas, uma conta
+# que sem a política poderia realizar estas operações (exemplos: put_object, delete_object,
+# get_object), quando executadas num objeto que é alvo de uma política contendo o `Effect` `Deny`,
+# falham com o erro `AccessDeniedByPolicy`, como demonstra o teste a seguir:
 
 number_clients = 2
-
 @pytest.mark.parametrize('multiple_s3_clients, bucket_with_one_object_policy, boto3_action', [
     ({"number_clients": number_clients}, {"policy_dict": policy_dict_template, "actions": "s3:PutObject", "effect": "Deny"}, 'put_object'),
     ({"number_clients": number_clients}, {"policy_dict": policy_dict_template, "actions": "s3:GetObject", "effect": "Deny"}, 'get_object'),
     ({"number_clients": number_clients}, {"policy_dict": policy_dict_template, "actions": "s3:DeleteObject", "effect": "Deny"}, 'delete_object')
 ], indirect = ['multiple_s3_clients', 'bucket_with_one_object_policy'])
-
-# ## Asserting if the owner has permissions blocked from own bucket
 def test_denied_policy_operations_by_owner(s3_client, bucket_with_one_object_policy, boto3_action):
     bucket_name, object_key = bucket_with_one_object_policy
     kwargs = {
@@ -120,7 +144,7 @@ def test_denied_policy_operations_by_owner(s3_client, bucket_with_one_object_pol
 
     #PutObject needs another variable
     if boto3_action == 'put_object' :
-        kwargs['Body'] = 'The answer for everthong is 42'
+        kwargs['Body'] = 'The answer for everthing is 42'
         
     #retrieve the method passed as argument
     method = getattr(s3_client, boto3_action)
@@ -132,14 +156,18 @@ def test_denied_policy_operations_by_owner(s3_client, bucket_with_one_object_pol
         logging.info(f"Method error response:{e.response}")
         assert e.response['Error']['Code'] == 'AccessDeniedByBucketPolicy'
 
+# ## Permitir operações específicas em objetos
+#
+# Da mesma forma, uma política pode dar um acesso a uma conta para determinadas operações. Uma
+# conta que sem política normalmente seria barrada com um erro `403` para operações como put_object,
+# get_object, delete_object, etc, por meio de uma política com `Effect` `Allow` conseguem obter
+# sucesso (status `200`, `204` e similares) como demostra o teste abaixo:
 
 @pytest.mark.parametrize('multiple_s3_clients, bucket_with_one_object_policy, boto3_action, expected', [
     ({"number_clients": number_clients}, {"policy_dict": policy_dict_template, "actions": "s3:PutObject", "effect": "Allow", "Principal": "*"}, 'put_object', 200),
     ({"number_clients": number_clients}, {"policy_dict": policy_dict_template, "actions": "s3:GetObject", "effect": "Allow", "Principal": "*"}, 'get_object', 200),
     ({"number_clients": number_clients}, {"policy_dict": policy_dict_template, "actions": "s3:DeleteObject", "effect": "Allow", "Principal": "*"}, 'delete_object', 204)
 ], indirect = ['multiple_s3_clients', 'bucket_with_one_object_policy'])
-
-# ## Asserting if the owner has permissions allowed from own bucket
 def test_allow_policy_operations_by_owner(multiple_s3_clients, bucket_with_one_object_policy, boto3_action,expected):
     bucket_name, object_key = bucket_with_one_object_policy
 
@@ -150,7 +178,7 @@ def test_allow_policy_operations_by_owner(multiple_s3_clients, bucket_with_one_o
 
     #PutObject needs another variable
     if boto3_action == 'put_object' :
-        kwargs['Body'] = 'The answer for everthong is 42'
+        kwargs['Body'] = 'The answer for everthing is 42'
         
     #retrieve the method passed as argument
     method = getattr(multiple_s3_clients[0], boto3_action)
