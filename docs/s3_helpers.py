0ea15ae318d8bffd10e6c619c662c4898deae3a3
Fabricio C Zuardi <fabricio@fabricio.org>
2025-03-14 11:49:35 -0300

    new spec: mgc cli bucket policy basic usage
    This patch adds specs covering the basic usage of the bucket policy
    feature using the mgc cli.
    
    The examples in here should include the same examples on the other
    specs:
    
      - https://docs.magalu.cloud/docs/storage/object-storage/how-to/permissions/policies
      - https://github.com/MagaluCloud/s3-tester/blob/main/spec/091_bucket-policy_spec.sh

--- docs/s3_helpers.py
+++ docs/s3_helpers.py
@@ -35,7 +35,8 @@ def run_example(dunder_name, test_name, config="../params.example.yaml"):
             "--color", "no", 
             "--pytest-durations", "0",
             # "-s", 
-            # "--log-cli-level", "INFO",
+            # "-x",
+            "--log-cli-level", "INFO",
             f"{get_spec_path()}::{test_name}"
         ])
  
@@ -256,12 +257,13 @@ def change_policies_json(bucket, policy_args: dict, tenants: list) -> json:
     policy = policy_args['policy_dict']
     effect = policy_args['effect']
     actions = policy_args['actions']
+    object_key = policy_args.get("resource_key", "*")
     
     #change arguments inside of the policy dict
     policy["Statement"][0]["Effect"] = effect
     policy["Statement"][0]["Principal"] = tenants
     policy["Statement"][0]["Action"] = actions
-    policy["Statement"][0]["Resource"] = [bucket + "/*", bucket]
+    policy["Statement"][0]["Resource"] = [f"{bucket}/{object_key}", bucket]
         
     logging.info(f"POLICY: {json.dumps(policy)}")
     return json.dumps(policy)
