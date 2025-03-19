81a346bd4bfd363b1bbd5526b45af2b9a5f5e7ac
Luís Henrique Hergesel Lima <97770407+2xlhe@users.noreply.github.com>
2025-03-19 13:25:59 -0300

    Gh actions scrapper (#85)
    * Included parquet loading and saving to the built dfs. Fixed pdf output path. Fixed Scale of plot on report
    
    * added boto3 upload and download scripts as a mean to backup the built data
    
    * Fixed hard coded into flags
    
    added prometheus exporter to the dataclasses of time and test
    
    * Prometheus exporter to dataclasses ExecutionTime and ExecutionTests
    
    * Exporter file to prometheus
    
    * Change names to snake case
    
    * Fixed path bug on prometheusExporter.py
    
    * Added github actions scrapper class and adapted the method extract artifact info to adequate to new data
    
    * Fixed extract artifact info and run sh
    
    * changed workflow artifact output
    
    * Fixed dataclasses loading bug while creating executable for gitactions
    
    * added new gitignore and deleted output folder on main folder
    
    feat: add git artifacts extractor
    
    * Local extractor (#84)
    
    * feat: add git artifacts extractor
    
    * fix comment
    
    * feat: add git artifacts extractor
    
    * fix headers
    
    * Finished integration to local and  git
    
    * Changed workflow run to have the time format in the log files
    
    ---------
    
    Co-authored-by: Bruno Fernandes <54373093+folkzb@users.noreply.github.com>
    Co-authored-by: luis <henrique.luis@luizalabs.com>

--- reports/src/logDataclasses.py
+++ reports/src/logDataclasses.py
@@ -3,52 +3,51 @@ from typing import Optional
 import numpy as np
 import pandas as pd
 from arqManipulation import ArqManipulation as am
+import os
 
 @dataclass
 class ExecutionEntity:
-    Execution_Datetime: np.datetime64
-    Endpoint: Optional[str]
-    Run_Time: Optional[float]
+    execution_datetime: np.datetime64
+    endpoint: Optional[str]
 
 @dataclass
 class Artifact:
-    Name: str
-    Execution_Datetime: np.datetime64
+    name: str
+    execution_datetime: np.datetime64
 
 @dataclass
 class ArtifactInfo:
-    Artifact_Name: str
-    URL: Optional[str]   #url to the object stored on the cloud, can be null
-    Artifact_Type: str
-    File_Extension: str
+    artifact_name: str
+    url: Optional[str]   #url to the object stored on the cloud, can be null
+    artifact_type: str
+    file_extension: str
 
 @dataclass
 class Tests:
-    Artifact_Name: str
-    Name: str
-    Category: str
-    Status: str
-    Arguments: Optional[str]  # Sometimes details the arguments they receive 
-    Execution_Datetime: np.datetime64
+    artifact_name: str
+    name: str
+    category: str
+    status: str
+    arguments: Optional[str]  # Sometimes details the arguments they receive 
+    execution_datetime: np.datetime64
 
 @dataclass
 class ExecutionTime:
-    Execution_name: str  # Can be Test_Name, fixture, or function
-    Execution_type: str
-    Execution_Datetime: np.datetime64
-    Number_Runs: int
-    Avg_Time: float
-    Min_Time: float
-    Total_Time: float
+    execution_name: str  # Can be Test_Name, fixture, or function
+    execution_type: str
+    execution_datetime: np.datetime64
+    number_runs: int
+    avg_time: float
+    min_time: float
+    total_time: float
 
 @dataclass
 class Failures:
-    Artifact_Name: str
-    Test_Name: str
-    Execution_Datetime: np.datetime64
-    Error: str
-    Details: Optional[str]  # Retrieved pytest error description
-
+    artifact_name: str
+    test_name: str
+    execution_datetime: np.datetime64
+    error: str
+    details: Optional[str]  # Retrieved pytest error description
 
 class TestData:
     def __init__(self,
@@ -59,16 +58,44 @@ class TestData:
         execution_time: list[ExecutionTime],
         failures: list[Failures]
     ):
-        self.execution_entity = pd.DataFrame([asdict(execution_entity)])
-        self.artifact =pd.DataFrame([asdict(artifact)])
-        self.tests = pd.DataFrame(list(map(lambda t: asdict(t), tests)))
-        self.execution_time = pd.DataFrame(asdict(execution_time))
-        self.failures = pd.DataFrame(asdict(failures))
 
-    def load_existent(self) -> list[pd.DataFrame]:
-        """Load data from Parquet files for each attribute."""
-        attributes = vars(self)  # Get instance attributes as a dictionary
-        return [self.read_parquet_file(parquet_file_name=atb) for atb in attributes]
+        self.execution_entity = self.__list_to_df__(execution_entity)
+        self.artifact = self.__list_to_df__(artifact)
+        self.tests = self.__list_to_df__(tests[0])
+        self.execution_time = self.__list_to_df__(execution_time[0])
+        self.failures = self.__list_to_df__(failures[0])
+
+        self.load_existent()
+        self.save_loaded()
+
+    def __list_to_df__(self, input) -> list:
+        if isinstance(input, list):
+            return pd.DataFrame(list(map(lambda a: asdict(a), input)))
+        elif isinstance(input, pd.DataFrame) and input.empty:
+            return pd.DataFrame()
+        return pd.DataFrame(asdict(input))
+
+    def load_existent(self) -> None:
+        """
+        Load data from Parquet files for each attribute if the file exists and has data.
+        Merge the loaded DataFrame with the existing DataFrame in the attribute.
+        """
+        for atb in vars(self):
+            parquet_file_path = f"./output/{atb}.parquet"
+            if os.path.exists(parquet_file_path):  # Check if the file exists
+                loaded_df = am.read_parquet_file(parquet_file_path)
+                if not loaded_df.empty:  # Check if the loaded DataFrame is not empty
+                    existing_df = getattr(self, atb)  # 
+                    merged_df = pd.concat([existing_df, loaded_df], ignore_index=True)
+                    setattr(self, atb, merged_df)  
+        
+    def save_loaded(self):
+        for atb in vars(self):
+            df = getattr(self, atb)
+            if isinstance(df, pd.DataFrame) and not df.empty:
+                parquet_file_name = "./output/"+atb+".parquet"
+                am.save_df_to_parquet(df = df, parquet_file_name=parquet_file_name)  
+
 
 
 def get_fields(Dataclass: type) -> list[str]:
