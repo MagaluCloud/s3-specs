c410be324b1625a977f1e5ef280d12e68041f62b
Bruno Fernandes <54373093+folkzb@users.noreply.github.com>
2025-03-19 14:43:13 -0300

    merge exporter (#86)

--- bin/exporter.py
+++ bin/exporter.py
@@ -1,5 +1,6 @@
 from prometheus_client import start_http_server, Gauge
 import pandas as pd
+import argparse
 import time
 import glob
 import os
@@ -16,6 +17,23 @@ avg_gauge = Gauge(
     'Tempo médio de operações',
     ['region', 'tool', 'size', 'times', 'workers', 'quantity', 'operation','time']
 )
+
+execution_time_gauge = Gauge(
+    's3_specs_time_metrics', 
+    'Tests time metrics',  
+    ['execution_name', 'execution_type', 'time_metric']  
+)
+
+execution_status_gauge = Gauge(
+    's3_specs_status_data',  
+    'Test statuses', 
+    ['name', 'category']  
+)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--parquet_path',
+                    required=True, 
+                    help='Path of folder containing the execution_time and test parquet artifacts')
 
 def read_csv_and_update_metrics():
     report_folder = os.path.abspath("./report")
@@ -81,8 +99,62 @@ def read_csv_and_update_metrics():
     else:
         print("Nenhum arquivo benchmark_results.csv encontrado.")
 
+def execution_metrics_exporter():
+    file_path = 'execution_time.parquet'
+    file_path = os.path.join(parser.parquet_path, file_path)
+    df = pd.read_parquet(file_path)
+
+    # Drop unnecessary columns and take the first 5 rows
+    cleaned_time_metric_df = df.drop(columns=['execution_datetime', 'number_runs'])
+
+    # Melt the DataFrame
+    melted_df = pd.melt(
+        cleaned_time_metric_df,
+        id_vars=['execution_name', 'execution_type'],  
+        value_vars=['avg_time', 'min_time', 'total_time'],  
+        var_name='time_metric', 
+        value_name='time_values'  
+    ).reset_index(drop=True).drop_duplicates()
+
+    # Set gauge values
+    for record in melted_df.to_dict('records'):
+        execution_time_gauge.labels(
+            execution_name=record['execution_name'],
+            execution_type=record['execution_type'],
+            time_metric=record['time_metric']
+        ).set(record['time_values'])
+
+    print('Time metrics exported...')
+
+def test_metrics_exporter():
+    # Test data gauge
+    file_path = 'tests.parquet'
+    file_path = os.path.join(parser.parquet_path, file_path)
+    df = pd.read_parquet(file_path)
+
+    status_to_numeric = {
+        'PASSED': 1.0,
+        'FAILED': -1.0,
+        'ERROR': -2.0,
+    }
+
+    cleaned_status_df = df.drop(columns=['artifact_name', 'execution_datetime', 'arguments'])
+    cleaned_status_df['status'] = cleaned_status_df['status'].map(status_to_numeric).drop_duplicates()
+    dicts_time_metric_df = cleaned_status_df.to_dict(orient='records')
+
+    # Set gauge values
+    for record in dicts_time_metric_df:
+        execution_status_gauge.labels(
+            name=record['name'],  # Pass 'Name' label
+            category=record['category']  # Pass 'Category' label
+        ).set(record['status'])  # Set the numeric status value
+    print('Test metrics exported...')
+
+
 if __name__ == '__main__':
     start_http_server(8000)
     while True:
         read_csv_and_update_metrics()
+        test_metrics_exporter()
+        execution_metrics_exporter()
         time.sleep(600)  # Atualize a cada 600 segundos (10 minutos)
