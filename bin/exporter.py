44ccdd064d6b59d61955b11c7e52239effbd3027
Bruno Fernandes <bruno.fernandes1996@hotmail.com>
2025-03-19 16:04:51 -0300

    fix exceptions on exporter

--- bin/exporter.py
+++ bin/exporter.py
@@ -15,7 +15,7 @@ objs_consistency_time = Gauge(
 avg_gauge = Gauge(
     'objs_benchmark',
     'Tempo médio de operações',
-    ['region', 'tool', 'size', 'times', 'workers', 'quantity', 'operation','time']
+    ['region', 'tool', 'size', 'times', 'workers', 'quantity', 'operation', 'time']
 )
 
 execution_time_gauge = Gauge(
@@ -30,10 +30,14 @@ execution_status_gauge = Gauge(
     ['name', 'category']  
 )
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--parquet_path',
-                    required=True, 
-                    help='Path of folder containing the execution_time and test parquet artifacts')
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--parquet_path',
+                        required=True, 
+                        help='Path of folder containing the execution_time and test parquet artifacts')
+    return parser.parse_args()
+
+args = parse_args()
 
 def read_csv_and_update_metrics():
     report_folder = os.path.abspath("./report")
@@ -48,10 +52,8 @@ def read_csv_and_update_metrics():
         print(f'Processing file: {inconsistencies_file}')
         df = pd.read_csv(inconsistencies_file)
 
-        # Verificar o formato do CSV e processar adequadamente
         if 'command' in df.columns:
             for _, row in df.iterrows():
-                # Verificar se a linha contém valores válidos
                 if pd.notna(row['command']) and pd.notna(row['elapsed']):
                     labels = {
                         'quantity': str(row['quantity']),
@@ -62,7 +64,6 @@ def read_csv_and_update_metrics():
                         'elapsed': str(row['elapsed'])
                     }
                     objs_consistency_time.labels(**labels).set(row['elapsed'])
-
     else:
         print(f"Arquivo {inconsistencies_file} não encontrado.")
 
@@ -73,16 +74,11 @@ def read_csv_and_update_metrics():
         print(f'Processing file: {latest_processed_file}')
         df = pd.read_csv(latest_processed_file)
 
-        # Verificar o formato do CSV e processar adequadamente
         if 'operation' in df.columns:
-            # Agrupar pelos primeiros 7 campos e calcular a mediana do 'time'
             df_grouped = df.groupby(['region', 'tool', 'size', 'times', 'workers', 'quantity', 'operation'])['time'].median().reset_index()
-
-            # Exportar o arquivo agrupado com a mediana
             df_grouped.to_csv('report/resultado_grouped.csv', index=False)
             print("Arquivo 'resultado_grouped.csv' com as medianas gerado.")
 
-            # Processar os dados para o 'avg_gauge'
             for _, row in df_grouped.iterrows():
                 labels = {
                     'region': row['region'],
@@ -95,28 +91,32 @@ def read_csv_and_update_metrics():
                     'time': str(row['time'])
                 }
                 avg_gauge.labels(**labels).set(row['time'])
-
     else:
         print("Nenhum arquivo benchmark_results.csv encontrado.")
 
 def execution_metrics_exporter():
     file_path = 'execution_time.parquet'
-    file_path = os.path.join(parser.parquet_path, file_path)
-    df = pd.read_parquet(file_path)
+    file_path = os.path.join(args.parquet_path, file_path)
+    
+    try:
+        df = pd.read_parquet(file_path)
+    except FileNotFoundError:
+        print(f"Arquivo {file_path} não encontrado.")
+        return
 
-    # Drop unnecessary columns and take the first 5 rows
+    # Limpeza de colunas desnecessárias
     cleaned_time_metric_df = df.drop(columns=['execution_datetime', 'number_runs'])
 
-    # Melt the DataFrame
+    # Melt o DataFrame
     melted_df = pd.melt(
         cleaned_time_metric_df,
-        id_vars=['execution_name', 'execution_type'],  
-        value_vars=['avg_time', 'min_time', 'total_time'],  
-        var_name='time_metric', 
-        value_name='time_values'  
+        id_vars=['execution_name', 'execution_type'],
+        value_vars=['avg_time', 'min_time', 'total_time'],
+        var_name='time_metric',
+        value_name='time_values'
     ).reset_index(drop=True).drop_duplicates()
 
-    # Set gauge values
+    # Setar valores das métricas
     for record in melted_df.to_dict('records'):
         execution_time_gauge.labels(
             execution_name=record['execution_name'],
@@ -127,10 +127,14 @@ def execution_metrics_exporter():
     print('Time metrics exported...')
 
 def test_metrics_exporter():
-    # Test data gauge
     file_path = 'tests.parquet'
-    file_path = os.path.join(parser.parquet_path, file_path)
-    df = pd.read_parquet(file_path)
+    file_path = os.path.join(args.parquet_path, file_path)
+
+    try:
+        df = pd.read_parquet(file_path)
+    except FileNotFoundError:
+        print(f"Arquivo {file_path} não encontrado.")
+        return
 
     status_to_numeric = {
         'PASSED': 1.0,
@@ -142,14 +146,14 @@ def test_metrics_exporter():
     cleaned_status_df['status'] = cleaned_status_df['status'].map(status_to_numeric).drop_duplicates()
     dicts_time_metric_df = cleaned_status_df.to_dict(orient='records')
 
-    # Set gauge values
+    # Setar valores das métricas
     for record in dicts_time_metric_df:
         execution_status_gauge.labels(
-            name=record['name'],  # Pass 'Name' label
-            category=record['category']  # Pass 'Category' label
-        ).set(record['status'])  # Set the numeric status value
-    print('Test metrics exported...')
+            name=record['name'],
+            category=record['category']
+        ).set(record['status'])
 
+    print('Test metrics exported...')
 
 if __name__ == '__main__':
     start_http_server(8000)
