5cd2d71e37f669ae7acf92bba1f856a2f938fdd0
Luiz Antonio Rozendo <luiz.rozendo01@gmail.com>
2025-05-06 12:38:05 -0300

    add an exeption during finally deletion

--- src/s3_specs/docs/bulk_delete_test.py
+++ src/s3_specs/docs/bulk_delete_test.py
@@ -54,6 +54,9 @@ def test_delete_bucket_not_being_bucket_owner(get_bulk_s3_clients, create_bucket
         logging.info(exc_info)
         assert any(msg in str(exc_info.value) for msg in ["AccessDenied","AccessDeniedByBucketPolicy", "Access denied by bucket policy"])
     finally:
-        logging.info("deleting bucket")
-        response = owner.delete_bucket(Bucket=bucket_name)
-        logging.info(f"response: {response}")
+        try:
+            logging.info("deleting bucket")
+            response = owner.delete_bucket(Bucket=bucket_name)
+            logging.info(f"response: {response}")
+        except Exception as e:
+            logging.info(f"error during deletion: {e}")
